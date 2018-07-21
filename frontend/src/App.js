58d3f4bc852613103c6bc20a1d86fbd98e1e6bff
Otomakan <jgmisteli@gmail.com>
2018-07-21 13:17:32 -0500

    scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './assets/stylesheets/App.scss';
 
 class App extends Component {
 
@@ -23,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div class="btn">Hello</div>
       </div>
     );
   }
